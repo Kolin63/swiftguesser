6d85e7c034ebeeb43f2a4d084d2866abe303446f
Kolin63 <theverylastgoodname@gmail.com>
2024-12-23 23:58:04 -0500

    Change Fetch URL

--- leaderboard/main.js
+++ leaderboard/main.js
@@ -1,5 +1,5 @@
 document.getElementById('getleaderboard').addEventListener('click', function() {
-    fetch('https://swiftguesser.kolin63.com/leaderboard/')
+    fetch('https://www.swiftguesser.kolin63.com/leaderboard/leaderboard.json')
     .then(response => response.json())
     .then(data => {
         console.log('Leaderboard:', data);
