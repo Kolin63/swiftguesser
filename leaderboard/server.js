4230bc877f0d0e412607b7e8b136262d7b78ce8a
Kolin63 <theverylastgoodname@gmail.com>
2024-12-26 16:43:47 -0500

    Res.send

--- leaderboard/server.js
+++ leaderboard/server.js
@@ -17,6 +17,7 @@ const leaderboardPath = "~/leaderboard/leaderboard.json";
 
 // Endpoint to get leaderboard data
 app.get('/leaderboard/leaderboard.json', (req, res) => {
+    res.send("GET request");
     fs.readFile(leaderboardPath, 'utf8', (err, data) => {
         if (err) {
             console.error('Error reading leaderboard file:', err);
@@ -28,6 +29,7 @@ app.get('/leaderboard/leaderboard.json', (req, res) => {
 
 // Endpoint to update leaderboard data
 app.post('/leaderboard/leaderboard.json', (req, res) => {
+    res.send("POST request");
     const newData = req.body; // Data sent from the frontend
     fs.writeFile(leaderboardPath, JSON.stringify(newData, null, 2), (err) => {
         if (err) {
