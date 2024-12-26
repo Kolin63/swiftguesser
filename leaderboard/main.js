a4950cc538721b1178fd811e6f2692ee73087070
Kolin63 <theverylastgoodname@gmail.com>
2024-12-26 16:18:43 -0500

    Update main.js

--- leaderboard/main.js
+++ leaderboard/main.js
@@ -89,7 +89,6 @@ function makeLeaderboardJSON() {
         } 
     }
     console.log("makeLeaderboardJSON() finished", leaderboardData);
-    updateLeaderboard();
 }
 
 async function getWeight() {
