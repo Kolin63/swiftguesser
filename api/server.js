11d886c362f2ab6d23fe0c387e8cd05146470e84
Kolin63 <theverylastgoodname@gmail.com>
2024-12-27 00:46:19 -0500

    Body Parser

--- api/server.js
+++ api/server.js
@@ -10,8 +10,8 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
-app.use(express.json({ limit: '100mb' })); 
-app.use(express.urlencoded({ extended: true, limit: '100mb' }));
+app.use(bodyParser.json({limit: '1000mb'}));
+app.use(bodyParser.urlencoded({limit: '1000mb', extended: true}));
 app.use((req, res, next) => {
     console.log(`Request URL: ${req.url}`);
     console.log(`Content-Length: ${req.headers['content-length']}`);
