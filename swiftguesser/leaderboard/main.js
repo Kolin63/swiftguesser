92f19b54e14b92a2bb1b0a184758a20ad20ef49d
Kolin63 <theverylastgoodname@gmail.com>
2024-12-26 23:51:05 -0500

    Update main.js

--- swiftguesser/leaderboard/main.js
+++ swiftguesser/leaderboard/main.js
@@ -114,7 +114,7 @@ async function fetchLeaderboard() {
         return response.json();
     }) 
     .then(json => {
-        console.log("fetchLeaderboard() finished: ", JSON.parse(json));
+        console.log("fetchLeaderboard() finished: ", JSON.stringify(json));
         leaderboardData = json;
         makeLeaderboardJSON();
     });
