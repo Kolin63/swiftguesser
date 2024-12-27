ab0144f4f2aecdc759f5e4ccf77d0908c11aa33b
Kolin63 <theverylastgoodname@gmail.com>
2024-12-27 00:49:25 -0500

    Remove Extended

--- api/server.js
+++ api/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.json({ limit: '1000mb' })); 
-app.use(express.urlencoded({ extended: true, limit: '1000mb' }));
+app.use(express.urlencoded({ limit: '1000mb' }));
 app.use((req, res, next) => {
     console.log(`Request URL: ${req.url}`);
     console.log(`Content-Length: ${req.headers['content-length']}`);
