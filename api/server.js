9fe251bcecd9cebc3b5c58affe8588971967f76e
Kolin63 <theverylastgoodname@gmail.com>
2024-12-26 23:41:17 -0500

    JSON.parse

--- api/server.js
+++ api/server.js
@@ -18,7 +18,7 @@ const leaderboardPath = "leaderboard.json";
 app.get("/", (req, res) => {
     fs.readFile(leaderboardPath, function (err, data) {
         if (err) throw err;
-        res.json(JSON.stringify(data));
+        res.json(JSON.parse(data));
     });
 });
 
