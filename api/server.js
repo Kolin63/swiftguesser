4e4aefaa8d8fbbd3746478426005a1e67380434f
Kolin63 <theverylastgoodname@gmail.com>
2024-12-29 01:14:14 -0500

    Revert "Remove Server.js Debug"
    This reverts commit 46ef855c84202296f71a6857ab7c4344e2bd7b8f.

--- api/server.js
+++ api/server.js
@@ -12,6 +12,11 @@ app.use(express.json());
 
 app.use(express.json({ limit: '1000mb' })); 
 app.use(express.urlencoded({ limit: '1000mb' }));
+app.use((req, res, next) => {
+    console.log(`Request URL: ${req.url}`);
+    console.log(`Content-Length: ${req.headers['content-length']}`);
+    next();
+});
 
 app.use((req, res, next) => {
     const allowedOrigin = "https://swiftguesser.kolin63.com";
