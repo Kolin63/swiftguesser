5cfb01ea56003817f921e909d3b5f724f3bcbd2b
Kolin63 <theverylastgoodname@gmail.com>
2025-01-19 17:21:53 -0500

    Add Input to MP3 Cut Python

--- swiftguesser/swiftguessercutmp3.py
+++ swiftguesser/swiftguessercutmp3.py
@@ -3,6 +3,8 @@ import os
 from pydub import AudioSegment
 directory = "C:\\Users\\theve\\Downloads\\Swift Guesser Rename"
 
+input("Cutting to 60 seconds in " + directory + "\nPress any key to start")
+
 for file in os.listdir(directory):
     if (file.endswith(".mp3") == False): 
         continue
@@ -13,3 +15,6 @@ for file in os.listdir(directory):
 
     cutSong = song[:60000] # 60,000 ms, 60 seconds
     cutSong.export(filePath, format="mp3")
+
+
+input("Press any key to close")
