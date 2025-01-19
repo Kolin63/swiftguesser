de9a947f7ab2e80b9abf20921dfec0e89fa22617
Kolin63 <theverylastgoodname@gmail.com>
2025-01-19 14:08:14 -0500

    Add Set Your Name Popup

--- swiftguesser/navbar.js
+++ swiftguesser/navbar.js
@@ -1,4 +1,5 @@
 let changingName = false;
+let namePopupStatus = false;
 const styles = [
     "visibility:visible;background-color:#EEE",
     "visibility:hidden;padding:0;margin:0;height:0"
@@ -9,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const template = document.createElement("div");
 
     template.innerHTML = `
-        <div class="navbar">
+        <div class="navbar" id="navbar">
             <a href="/#top" title="Home">
                 <img src="/art/icon-wide-space.png" class="logo" alt="Swift Guesser" id="navbarlogo">
             </a>
@@ -62,14 +63,47 @@ document.addEventListener("DOMContentLoaded", function () {
             localStorage.setItem("playerName", playerName);
             console.log("new name:", playerName);
             updateChangingName(nametext, namechange);
+            if (playerName != "NUL" && namePopupStatus) {
+                namePopupStatus = false;
+                namePopupWrapper.style.visibility = "hidden";
+                namechange.style.position = "static";
+                namePopup.style.scale = "0";
+            }
         }
     })
 
+    const namePopupWrapper = document.createElement("div");
+    namePopupWrapper.className = "popup-wrapper";
+    namePopupWrapper.id = "name-popup-wrapper";
+    namePopupWrapper.style.zIndex = "1";
+    namePopupWrapper.style.visibility = "hidden";
+    namePopupWrapper.innerHTML = `
+        <div id="name-popup-background" class="popup-background"></div>
+        <div id="name-popup" class="popup">
+            <h1 id="name-popup-title" class="popup-title" style="margin: 50px 0 0 0">Set Your Name</h1>
+            <p class="popup-info">Must be 3 characters long</p>
+            <p class="popup-info">Will be shown on the leaderboard</p>
+            <p class="popup-info">Can be changed at any time in the top-right corner</p>
+            <div id="navbar-namechange-target"></div>
+        </div> `;
+    document.body.appendChild(namePopupWrapper);
+    const namePopup = document.getElementById("name-popup");
+    namePopup.style.scale = "1";
+
     setInterval(function () {
-        if (playerName == "NUL" && nametext.style.backgroundColor == "rgb(238, 238, 238)") {
-            nametext.style.backgroundColor = "rgb(255, 0, 0)"
-        } else {
-            nametext.style.backgroundColor = "rgb(238, 238, 238)"
+        if (playerName == "NUL" && !namePopupStatus) {
+            namePopupStatus = true;
+            updateChangingName(nametext, namechange);
+            namePopupWrapper.style.visibility = "visible";
+            namechange.focus();
+            namePopup.style.scale = "1";
+        }
+        if (playerName == "NUL" && namePopupStatus) {
+            const targetRect = document.getElementById("navbar-namechange-target").getBoundingClientRect();
+            namechange.style.position = "absolute";
+            namechange.style.left = (targetRect.left + 400 / 2 - 94 / 2) + "px";
+            namechange.style.top = (targetRect.top + 20) + "px";
+            document.getElementById("navbar").style.zIndex = "10";
         }
     }, 1000)
 
